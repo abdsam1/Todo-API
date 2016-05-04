f526f6dc3a1ffe0ab9e9d3e5f1897e17f0b5f188
Abdul Samad <samad.imtiaz92@gmail.com>
2016-05-04 12:59:30 +0500

    Added new requests for adding Tdoo items and deleting as well.

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var _ = require('underscore');
 var app = express();
 var port = process.env.PORT || 3000;
-var todoNext=1;
+var todoNext=0;
 var todos = [];
 
 app.use(bodyParser.json());//When a JSON request comes in
@@ -19,12 +20,7 @@ app.get('/todos',function(req,res){
 
 app.get('/todos/:id',function(req,res){
 	var todoId = parseInt(req.params.id);
-	var matchedTodoItem;
-	todos.forEach(function(element){
-		if(element.id === todoId){
-			matchedTodoItem=element;
-		}
-	});
+	var matchedTodoItem = _.findWhere(todos,{id:todoId});
 		if(matchedTodoItem){
 		res.json(matchedTodoItem);
 		}
@@ -35,12 +31,33 @@ app.get('/todos/:id',function(req,res){
 
 //POST /todos
 app.post('/todos',function(req,res){
-	body = req.body;
-	console.log('description ' + body.description);
-	body.id = todoNext++;
+	body = _.pick(req.body,'description','completed');
+	if(!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length ===0){
+		return res.status(400).send();
+	}
+	body.description = body.description.trim();
+	console.log('description: ' + body.description);
+	body.id = ++todoNext;
 	todos.push(body);
 	res.json(body);
 });
+
+//DELETE /todos/:id
+
+app.delete('/todos/:id',function(req,res){
+	var todoToBeDeletedId = parseInt(req.params.id);
+	var toBeDeleted = _.findWhere(todos,{id:todoToBeDeletedId});
+	if(toBeDeleted){
+		todos = _.without(todos,toBeDeleted);
+		console.log('Deleted Todo: '+ toBeDeleted.description);
+		res.json(toBeDeleted);
+	}
+	else{
+		res.status(400).send();
+	}
+});
+
+
 
 app.listen(port,function(){
 	console.log('Express Server running on port ' + port);
