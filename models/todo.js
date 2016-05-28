0c7dfbf556dc05725dcef89f307ead5bea2b7d43
Abdul Samad <samad.imtiaz92@gmail.com>
2016-05-28 23:56:09 +0500

    Add todo model and POST /todos

---
+++ models/todo.js
@@ -0,0 +1,17 @@
+module.exports = function(sequelize, DataTypes) {
+	return sequelize.define('todo', {
+		description: {
+			type: DataTypes.STRING,
+			allowNull: false,
+			validate: {
+				notEmpty: true
+			}
+		},
+		completed: {
+			type: DataTypes.BOOLEAN,
+			allowNull: false,
+			defaultValue: false
+		}
+	});
+
+};
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var _ = require('underscore');
+var db = require('./db.js');
 var app = express();
 var port = process.env.PORT || 3000;
 var todoNext = 0;
@@ -51,14 +52,20 @@ app.get('/todos/:id', function(req, res) {
 //POST /todos
 app.post('/todos', function(req, res) {
 	var body = _.pick(req.body, 'description', 'completed');
-	if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
-		return res.status(400).send();
-	}
-	body.description = body.description.trim();
-	console.log('description: ' + body.description);
-	body.id = ++todoNext;
-	todos.push(body);
-	res.json(body);
+	db.todo.create(body).then(function(todo) {
+		res.json(todo.toJSON());
+	}).catch(function(e) {
+		res.status(400).json('wrong');
+	});
+
+	// if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
+	// 	return res.status(400).send();
+	// }
+	// body.description = body.description.trim();
+	// console.log('description: ' + body.description);
+	// body.id = ++todoNext;
+	// todos.push(body);
+	// res.json(body);
 });
 
 //DELETE /todos/:id
@@ -103,6 +110,12 @@ app.put('/todos/:id', function(req, res) {
 	res.json(matchedTodoItem);
 });
 
-app.listen(port, function() {
-	console.log('Express Server running on port ' + port);
+db.sequelize.sync({
+	force: true
+}).then(function() {
+	app.listen(port, function() {
+		console.log('Express Server running on port ' + port);
+	});
+}).catch(function(e) {
+	console.log(e.toJSON());
 });
